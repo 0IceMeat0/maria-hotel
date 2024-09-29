b1f23785b9bb8837f4b240ce91303ec4cb66608c
0IceMeat0 <icemeat2004@gmail.com>
2024-09-29 21:52:08 +0300

    remove lazy loading

--- src/shared/routing/routing.js
+++ src/shared/routing/routing.js
@@ -1,52 +1,25 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
-import ErrorBoundary from '../ui/error-boundary';
-import LoadingSkeleton from '../ui/skeleton/loading-skeleton';
-
-const Hotel = React.lazy(() => import('@/pages/hotel/hotel'));
-const HotelHelper = React.lazy(
-    () => import('@/pages/hotel-helper/hotel-helper'),
-);
-const DinamicArendaComponent = React.lazy(
-    () => import('@/widgets/hotel-helper/rent/ui/dynamic-rent'),
-);
-
-const ErrorBoundaryLayout = ({ children, fallback }) => {
-    return (
-        <ErrorBoundary>
-            <Suspense fallback={fallback}>{children}</Suspense>
-        </ErrorBoundary>
-    );
-};
+import Hotel from '@/pages/hotel/hotel';
+import HotelHelper from '@/pages/hotel-helper/hotel-helper';
+import DinamicArendaComponent from '@/widgets/hotel-helper/rent/ui/dynamic-rent';
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: (
-            <ErrorBoundaryLayout fallback={<LoadingSkeleton />}>
-                <Hotel />
-            </ErrorBoundaryLayout>
-        ),
+        element: <Hotel />,
     },
     {
         path: 'qrcode',
         children: [
             {
                 index: true,
-                element: (
-                    <ErrorBoundaryLayout fallback={<LoadingSkeleton />}>
-                        <HotelHelper />
-                    </ErrorBoundaryLayout>
-                ),
+                element: <HotelHelper />,
             },
             {
                 path: ':id',
-                element: (
-                    <ErrorBoundaryLayout fallback={<LoadingSkeleton />}>
-                        <DinamicArendaComponent />
-                    </ErrorBoundaryLayout>
-                ),
+                element: <DinamicArendaComponent />,
             },
         ],
     },
--- src/shared/ui/skeleton/loading-skeleton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LoadingSkeleton = () => {
-    return <div>Loading...</div>;
-};
-
-export default LoadingSkeleton;
