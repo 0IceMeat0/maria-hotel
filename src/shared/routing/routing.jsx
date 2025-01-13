3ea74b5be4349a7bdfcc800ea1a601ea2f0d9ec0
Даниил Мамулян <danil@Daniils-MacBook-Pro.local>
2025-01-13 10:15:57 +0300

    release 1.24

--- src/shared/routing/routing.jsx
+++ src/shared/routing/routing.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
 import { ErrorBoundaryLayout } from '../error-boundary';
 import { HotelSkeleton, GallerySkeleton } from '../ui/skeleton';
 
-const Hotel = React.lazy(() => import('@/pages/hotel/hotel'));
-const HotelHelper = React.lazy(
-    () => import('@/pages/hotel-helper/hotel-helper'),
-);
-const DinamicArendaComponent = React.lazy(
+// Используем lazy-загрузку для всех страниц
+const Hotel = lazy(() => import('@/pages/hotel/hotel'));
+const HotelHelper = lazy(() => import('@/pages/hotel-helper/hotel-helper'));
+const DynamicRent = lazy(
     () => import('@/widgets/hotel-helper/rent/ui/dynamic-rent'),
 );
-const Gallery = React.lazy(
+const Gallery = lazy(
     () => import('../../widgets/hotel/gallery/ui/gallery.jsx'),
 );
+
 export const router = createBrowserRouter([
     {
         path: '/',
@@ -23,38 +23,33 @@ export const router = createBrowserRouter([
             </ErrorBoundaryLayout>
         ),
     },
-    // {
-    //     path: 'qrcode',
-    //     children: [
-    //         {
-    //             index: true,
-    //             element: (
-    //                 <ErrorBoundaryLayout fallback={<HotelSkeleton />}>
-    //                     <HotelHelper />
-    //                 </ErrorBoundaryLayout>
-    //             ),
-    //         },
-    //         {
-    //             path: ':id',
-    //             element: (
-    //                 <ErrorBoundaryLayout fallback={<HotelSkeleton />}>
-    //                     <DinamicArendaComponent />
-    //                 </ErrorBoundaryLayout>
-    //             ),
-    //         },
-    //     ],
-    // },
     {
         path: 'gallery',
-        children: [
-            {
-                index: true,
-                element: (
-                    <ErrorBoundaryLayout fallback={<GallerySkeleton />}>
-                        <Gallery />
-                    </ErrorBoundaryLayout>
-                ),
-            },
-        ],
+        element: (
+            <ErrorBoundaryLayout fallback={<GallerySkeleton />}>
+                <Gallery />
+            </ErrorBoundaryLayout>
+        ),
     },
 ]);
+// {
+//     path: 'qrcode',
+//     children: [
+//         {
+//             index: true,
+//             element: (
+//                 <ErrorBoundaryLayout fallback={<HotelSkeleton />}>
+//                     <HotelHelper />
+//                 </ErrorBoundaryLayout>
+//             ),
+//         },
+//         {
+//             path: ':id',
+//             element: (
+//                 <ErrorBoundaryLayout fallback={<HotelSkeleton />}>
+//                     <DynamicRent />
+//                 </ErrorBoundaryLayout>
+//             ),
+//         },
+//     ],
+// },
