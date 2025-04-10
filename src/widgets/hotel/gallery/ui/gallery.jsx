48cc473340e73d0df8bc0eec0936032d21611cfb
Даниил Мамулян <danil@Daniils-MacBook-Pro.local>
2025-04-10 20:11:31 +0300

    release 1.28

--- src/widgets/hotel/gallery/ui/gallery.jsx
+++ src/widgets/hotel/gallery/ui/gallery.jsx
@@ -46,7 +46,7 @@ const Gallery = () => {
                 Забронировать
             </Link> */}
             <a
-                href="https://wa.me/79320121253?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%2C%20%D1%85%D0%BE%D1%82%D0%B5%D0%BB%D0%B8%20%D0%B1%D1%8B%20%D0%B7%D0%B0%D0%B1%D1%80%D0%BE%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%20%D0%B2%D0%B0%D1%88%20%D0%B4%D0%BE%D0%BC%D0%B8%D0%BA"
+                href="https://bronirui.online/lesnoi-pereulok"
                 className={styles.buttonBron}
             >
                 Забронировать
